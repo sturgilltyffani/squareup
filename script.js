3644d2075bac66ade4ad3117b6969f3587cf104a
sturgilltyffani <sturgilltyffani@gmail.com>
2017-10-14 22:54:07 -0500

    SOS

---
+++ script.js
@@ -0,0 +1,48 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var recBtn = document.getElementById('btn1');
+var cirBtn = document.getElementById('btn2');
+var squBtn = document.getElementById('btn3');
+var triBtn = document.getElementById('btn4');
+var Shape = /** @class */ (function () {
+    function Shape() {
+    }
+    return Shape;
+}());
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Circle;
+}(Shape));
+var Triangle = /** @class */ (function (_super) {
+    __extends(Triangle, _super);
+    function Triangle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Triangle;
+}(Shape));
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Rectangle;
+}(Shape));
+var Square = /** @class */ (function (_super) {
+    __extends(Square, _super);
+    function Square() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Square;
+}(Shape));
+// SOS SOS SOS SOS 
---
+++ script.ts
@@ -0,0 +1,27 @@
+const recBtn = document.getElementById('btn1');
+const cirBtn = document.getElementById('btn2');
+const squBtn = document.getElementById('btn3');
+const triBtn = document.getElementById('btn4');
+
+class Shape {
+    
+}
+
+class Circle extends Shape {
+    cirBtn: number;
+}
+
+class Triangle extends Shape { 
+    triBtn: number;
+}
+
+class Rectangle extends Shape {
+    recBtn: number;
+}
+
+class Square extends Shape {
+    squBtn: number;
+}
+
+
+// SOS SOS SOS SOS
